4eb1a5766f1efb858c8e2bce9b3f6a3d01e9b438
Your Name <you@example.com>
2019-04-25 17:38:07 +0800

    add portlet and custom_folder_listing

---
+++ src/ncue/content/browser/views.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.contenttypes.browser.folder import FolderView
+from ncue.content import _
+from zope.interface import alsoProvides
+from plone.protect.interfaces import IDisableCSRFProtection
+# from email.mime.text import MIMEText
+import json
+import datetime
+# from db.connect.browser.views import SqlObj
+#from db.connect.browser.base_inform_configlet import IInform
+# from Products.CMFPlone.utils import getSiteLogo
+# import time
+
+
+class Debug(BrowserView):
+    def __call__(self):
+        import pdb; pdb.set_trace()
