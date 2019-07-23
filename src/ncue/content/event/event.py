15bcac01d71eaabd9e7e923b16ab1bf069df391a
Andy Fang <andy@mingtak.com.tw>
2019-07-23 11:42:05 +0800

    modify event.py

--- src/ncue/content/event/event.py
+++ src/ncue/content/event/event.py
@@ -6,8 +6,10 @@ def moveObjectsToTop(obj, event):
     """
     Moves Object to the top of its folder
     """
-
-    folder = obj.getParentNode()
+    try:
+        folder = obj.getParentNode()
+    except:
+        return
     if folder is not None and hasattr(folder, 'moveObjectsToTop'):
         if obj.portal_type in ['Document', 'News Item', 'Event']:
             folder.moveObjectsToTop(obj.id)
